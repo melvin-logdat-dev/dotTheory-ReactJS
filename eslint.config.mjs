098165d7c039c95b5be89e72237abfa6d047fe77
melvin-logdat-dev <melvin.logdat@codeandtheory.com>
2026-02-20 11:12:16 +0800

    Fixed config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,11 @@
-// import js from "@eslint/js";
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-// import pluginReact from "eslint-plugin-react";
-// import { defineConfig } from "eslint/config";
-
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-//   tseslint.configs.recommended,
-//   pluginReact.configs.flat.recommended,
-// ]);
 import js from "@eslint/js";
 import globals from "globals";
-import jsdoc from "eslint-plugin-jsdoc";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js, jsdoc },
-
-    languageOptions: {
-      globals: globals.browser,
-    },
-
-    rules: {
-      // Add JSDoc recommended rules manually
-      ...jsdoc.configs.recommended.rules,
-
-      // Add or override JSDoc rules here
-      "jsdoc/require-jsdoc": [
-        "warn",
-        {
-          publicOnly: true,
-          require: {
-            FunctionDeclaration: true,
-            MethodDefinition: true,
-            ClassDeclaration: true,
-            ArrowFunctionExpression: false,
-            FunctionExpression: false,
-          },
-        },
-      ],
-    },
-  },
-
-  {
-    files: ["**/*.js"],
-    // languageOptions: { sourceType: "script" },
-    languageOptions: { sourceType: "module" },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
