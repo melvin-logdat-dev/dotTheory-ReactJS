49def6b5ab5963f3a6f91f7500446483f4a82320
melvin-logdat-dev <melvin.logdat@codeandtheory.com>
2026-03-05 20:31:32 +0800

    RCT-202: React Router Basics

---
+++ src/RCT-202/src/components/Featured.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Featured = () => {
+  let { name, topic } = useParams();
+  name = name.replace("-", " ");
+
+  return (
+    <div className="main-content">
+      <h2>{name}</h2>
+      <p>
+        Introducing <strong>{name}</strong>, a teacher who loves teaching
+        courses about <strong>{topic}</strong>!
+      </p>
+    </div>
+  );
+};
+
+export default Featured;
